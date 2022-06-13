ddd055be97236e687459c8a9a1b245695e31faff
Shiva Menta <shiva.123.menta@gmail.com>
2022-06-13 02:14:10 +0200

    Add resizing slider functionality

--- js-file.js
+++ js-file.js
@@ -1,7 +1,8 @@
 
 const drawingContainer = document.querySelector('.grid-container');
+let defaultSize = 250;
 
-function createGrid(size=16) {
+function createGrid(size=50) {
     drawingContainer.setAttribute('style', 'grid-template-columns: ' + 
         '1fr '.repeat(size));
     
@@ -10,17 +11,31 @@ function createGrid(size=16) {
             const cell = document.createElement('div');
             cell.classList.add('grid-item');
             drawingContainer.appendChild(cell);
-
+            cell.setAttribute('style', scaledDivSize(size));
         }
     }
+
+    const drawingDivs = document.querySelectorAll('.grid-item');
+    drawingDivs.forEach((squareDiv) => {
+        squareDiv.addEventListener('mouseover', () => {
+            squareDiv.setAttribute('style', 'background-color: black');
+        })
+    });
 }
 
+function scaledDivSize(size){
+    let pixelFloat = defaultSize / size;
+    return 'height: ' + pixelFloat + 'px; width: ' + pixelFloat + 'px'
+}
 
+createGrid(50);
 
-createGrid(16);
-const drawingDivs = document.querySelectorAll('.grid-item');
-drawingDivs.forEach((squareDiv) => {
-    squareDiv.addEventListener('mouseover', () => {
-        squareDiv.setAttribute('style', 'background-color: black');
-    })
-});
+var slider = document.getElementById("sizeSliderRange");
+var sizeOutput = document.getElementById("size-output");
+sizeOutput.innerHTML = slider.value + ' x ' + slider.value;
+
+slider.oninput = function() {
+    sizeOutput.innerHTML = slider.value + ' x ' + slider.value;
+    drawingContainer.innerHTML = '';
+    createGrid(slider.value);
+}
